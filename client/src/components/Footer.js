9c163940e1555a8b8c66a9cf9dcfdbe7c05e965d
Bryce Speirs <baspeirs@gmail.com>
2024-01-12 13:12:24 -0500

    background for footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,28 +4,35 @@ import GitHubIcon from '../images/SocialMediaIcons/icons8-github-48.png';
 import LinkedinIcon from '../images/SocialMediaIcons/icons8-linkedin-48.png';
 import PortfolioIcon from '../images/SocialMediaIcons/icons8-source-code-48.png';
 import EmailIcon from '../images/SocialMediaIcons/icons8-email-48.png';
+import Background from '../images/OtherImages/AdminsBackground.png';
 
 export const Footer = () => {
 
     return (
-        <footer id="footer">
-                <h5>Bryce Speirs Development &copy; 2024</h5>
-                    <a href='https://github.com/baspeirs'>
-                        <img src={GitHubIcon} alt="github" />
-                        GitHub
-                    </a>
-                    <a href='https://www.linkedin.com/in/bryce-speirs-bb1319199/'>
-                        <img src={LinkedinIcon} alt="linkedin" />
-                        LinkedIn
-                    </a>
-                    <a href='https://baspeirs.github.io/'>
-                    <img src={PortfolioIcon} alt="portfolio" />
-                        Portfolio
-                    </a>
-                    <a href='mailto: baspeirs@gmail.com'>
-                    <img src={EmailIcon} alt="email" />
-                        Contact
-                    </a>
+        <footer id="footer" style={
+            {
+                backgroundImage: `url(${Background})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat'
+            }
+        }>
+            <h5>Bryce Speirs Development &copy; 2024</h5>
+            <a href='https://github.com/baspeirs'>
+                <img src={GitHubIcon} alt="github" />
+                GitHub
+            </a>
+            <a href='https://www.linkedin.com/in/bryce-speirs-bb1319199/'>
+                <img src={LinkedinIcon} alt="linkedin" />
+                LinkedIn
+            </a>
+            <a href='https://baspeirs.github.io/'>
+                <img src={PortfolioIcon} alt="portfolio" />
+                Portfolio
+            </a>
+            <a href='mailto: baspeirs@gmail.com'>
+                <img src={EmailIcon} alt="email" />
+                Contact
+            </a>
         </footer>
     )
 };
